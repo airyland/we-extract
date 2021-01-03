6117d86f43f748b21acf35416b6ae4c39c7099c5
airyland <airyland@qq.com>
2021-01-04 07:46:17 +0800

    file clean

--- test_profile.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-const extract = require('./extract-profile')
-
-const html = fs.readFileSync('./links/profile.html', 'utf-8')
-
-const rs = extract(html)
-console.log(JSON.stringify(rs, null, 2))
